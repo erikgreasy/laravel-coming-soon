1cfc0918f24b0f72f9cd052e094326b8a1c24323
Erik Masny <erik.masny@gmail.com>
2024-01-25 20:37:27 +0100

    feat: use route from config

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Erikgreasy\LaravelComingSoon\Http\Controller\ComingSoonController;
 use Illuminate\Support\Facades\Route;
 
 if (config('coming-soon.enabled')) {
-    Route::get('coming-soon', ComingSoonController::class)->name('coming-soon');
+    Route::get(config('coming-soon.route'), ComingSoonController::class)->name('coming-soon');
 }
